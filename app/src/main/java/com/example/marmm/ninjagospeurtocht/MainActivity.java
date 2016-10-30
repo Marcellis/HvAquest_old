f7b00cba8287ccf77e34274d3df17706b8e78700
Marcellis <m.m.c.m.marcellis@hva.nl>
2016-10-30 08:52:08 +0100

    Second screen added

--- app/src/main/java/com/example/marmm/ninjagospeurtocht/MainActivity.java
+++ app/src/main/java/com/example/marmm/ninjagospeurtocht/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.marmm.ninjagospeurtocht;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -11,34 +13,28 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    RadioGroup radiogroup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button = (Button) findViewById(R.id.buttonVraag);
 
-        String[] items = getResources().getStringArray(R.array.questions);
 
-        TextView vraag = (TextView) findViewById(R.id.textView);
-        radiogroup = (RadioGroup) findViewById(R.id.radioGroup);
-
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, VraagActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
-    public void onclickVolgende (View view){
 
 
-        RadioButton selectRadio = (RadioButton) findViewById(radiogroup.getCheckedRadioButtonId());
-        String opinion = selectRadio.getText().toString();
-        Toast.makeText(this, opinion, Toast.LENGTH_SHORT).show();
 
-    }
-
-
-    public void onclickVorige (View view){
-        Toast.makeText(this, "vorige", Toast.LENGTH_SHORT).show();
-    }
 
 }
---
+++ app/src/main/java/com/example/marmm/ninjagospeurtocht/VraagActivity.java
@@ -0,0 +1,78 @@
+package com.example.marmm.ninjagospeurtocht;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class VraagActivity extends AppCompatActivity {
+
+    RadioGroup radiogroup;
+    Button antwoordButton;
+    String[] items;
+    TextView vraag;
+    String juisteAntwoord;
+    RadioButton antwoord;
+    RadioButton antwoord1;
+    RadioButton antwoord2;
+    RadioButton antwoord3;
+    AlertDialog.Builder alertDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vraag);
+
+        antwoordButton = (Button) findViewById(R.id.buttonAntwoord);
+        vraag = (TextView) findViewById(R.id.textVraag);
+        vraag.setText(getString(R.string.vraag1));
+
+
+        items = getResources().getStringArray(R.array.antwoorden1);
+
+        radiogroup = (RadioGroup) findViewById(R.id.radioGroup);
+        antwoord1 = (RadioButton) findViewById(R.id.radioButton1);
+        antwoord2 = (RadioButton) findViewById(R.id.radioButton2);
+        antwoord3 = (RadioButton) findViewById(R.id.radioButton3);
+
+        antwoord1.setText(items[1]);
+        antwoord2.setText(items[2]);
+        antwoord3.setText(items[3]);
+
+        juisteAntwoord = items[0];
+
+
+        alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Antwoord");
+
+
+        antwoordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int selectedId=radiogroup.getCheckedRadioButtonId();
+                antwoord = (RadioButton) findViewById(selectedId);
+                if ((antwoord.getText().toString()).equals(juisteAntwoord)) {
+                    alertDialog.setMessage("Antwoord is juist");
+                    alertDialog.show();
+                }
+
+                   else {
+                    alertDialog.setMessage("Antwoord is NIET juist");
+                    alertDialog.show();
+                }
+
+            }
+        });
+
+
+
+    }
+
+
+
+}
